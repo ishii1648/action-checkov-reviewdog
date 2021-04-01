72a4253579a3e525eaecc1f7a1bfd53ece54e9b0
ishii1648 <ishiisho0509@gmail.com>
2021-04-02 05:57:24 +0900

    add return code
    Signed-off-by: ishii1648 <ishiisho0509@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,8 @@ checkov -d $INPUT_WORKING_DIRECTORY $SKIP_CHECK_FLAG -o json \
     | python3 /parse.py \
     | reviewdog -efm="%f:%l: %m" -name="checkov" -reporter="${INPUT_REPORTER}" -fail-on-error="${INPUT_FAIL_ON_ERROR}" -filter-mode="${INPUT_FILTER_MODE}"
 
-exit_code=$?
-echo $exit_code
+checkov_return="${PIPESTATUS[0]}" reviewdog_return="${PIPESTATUS[1]}" exit_code=$?
+echo ::set-output name=checkov-return-code::"${checkov_return}"
+echo ::set-output name=reviewdog-return-code::"${reviewdog_return}"
 
 exit $exit_code
